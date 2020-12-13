15bf905fa6722fbb4227864052411680a75f6ac6
Meme Man <tomekslawny@gmail.com>
2020-12-13 21:48:24 +0100

    Input Select TextArea Commit

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -1,23 +1,54 @@
 import React from "react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
+import FormikControl from "./FormikControl";
 
 function FormikContainer() {
-  const initialValues = {};
-
-  const validationSchema = Yup.object({});
-
-  onSubmit = (values) => console.log("form data", values);
+  const dropdownOptions = [
+    { key: "Select an option", value: "" },
+    { key: "Option 1", value: "option1" },
+    { key: "Option 2", value: "option2" },
+    { key: "Option 3", value: "option3" },
+  ];
+  const initialValues = {
+    email: "",
+    description: "",
+    selectOption: "",
+  };
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Required"),
+    description: Yup.string().required("Required"),
+    selectOption: Yup.string().required("Required"),
+  });
+  const onSubmit = (values) => console.log("Form data", values);
 
   return (
     <div>
-      <Formik>
+      <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit={onSubmit}
+      >
         {(formik) => (
           <Form>
-            <submit type="submit">Submit</submit>
+            <FormikControl
+              control="input"
+              type="email"
+              label="Email"
+              name="email"
+            />
+            <FormikControl
+              control="textarea"
+              label="Description"
+              name="description"
+            />
+            <FormikControl
+              control="select"
+              label="Select a topic"
+              name="selectOption"
+              options={dropdownOptions}
+            />
+            <button type="submit">Submit</button>
           </Form>
         )}
       </Formik>
---
+++ src/components/FormikControl.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Input from "./Input";
+import Select from "./Select";
+import Textarea from "./Textarea";
+
+const FormikControl = (props) => {
+  const { control, ...rest } = props;
+  switch (control) {
+    case "input":
+      return <Input {...rest} />;
+    case "textarea":
+      return <Textarea {...rest} />;
+    case "select":
+      return <Select {...rest} />;
+    case "radio":
+    case "checkbox":
+    case "date":
+    default:
+      return null;
+  }
+};
+
+export default FormikControl;
