3ab92f16ab1bd751825700b55867872483ead215
Meme Man <tomekslawny@gmail.com>
2020-12-14 21:01:19 +0100

    Login Registration Form Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
 import FormikContainer from "./components/FormikContainer";
+import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
+      <LoginForm />
+      <hr />
+      <RegistrationForm />
+      <hr />
       <FormikContainer />
     </div>
   );
